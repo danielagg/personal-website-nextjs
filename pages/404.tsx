9bd91f5ff38729211495d5b356e0703842c8ddb9
Daniel Agg <daniel.agg@outlook.com>
2022-06-18 13:38:29 +0200

    illegal character fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ const Custom404: NextPage = () => {
   return (
     <main className="min-h-screen w-full flex items-center justify-center dark:bg-slate-900 text-gray-700 dark:text-slate-400">
       <div className="flex flex-col items-center justify-center space-y-6">
-        <div className="text-8xl font-bold">That's a 404 :(</div>
+        <div className="text-8xl font-bold">That&apos;s a 404 :(</div>
         <div className="text-2xl">
           Sorry, the page you are looking for does not exist.
         </div>
