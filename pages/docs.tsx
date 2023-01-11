43fa9fa4fa475a1939a841483e5083057253a56a
Daniel Agg <daniel.agg@outlook.com>
2023-01-11 11:17:21 +0100

    cleanup

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -3,14 +3,7 @@ import type { NextPage } from "next";
 const Docs: NextPage = () => {
   return (
     <div className="font-heebo text-gray-700 scroll-smooth">
-      <div onClick={() => window.open("/docs/1.pdf")}>1st</div>
-      <div onClick={() => window.open("/docs/2.pdf")}>2nd</div>
-      <div onClick={() => window.open("/docs/3.pdf")}>3rd</div>
-      <div onClick={() => window.open("/docs/4.pdf")}>4th</div>
-      <div onClick={() => window.open("/docs/5.pdf")}>5th</div>
-      <div onClick={() => window.open("/docs/6.pdf")}>6th</div>
-      <div onClick={() => window.open("/docs/7.pdf")}>6th</div>
-      <div onClick={() => window.open("/docs/8.pdf")}>6th</div>
+      <div onClick={() => window.open("/docs/boarding.pdf")}>1st</div>
     </div>
   );
 };
