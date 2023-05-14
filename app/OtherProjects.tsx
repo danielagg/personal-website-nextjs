2e67102afee011a12b4e5e4137a8b7cd0845b088
Daniel Agg <daniel.agg@outlook.com>
2023-05-14 19:51:19 +0200

    responsive design on CV/Blog buttons

--- app/OtherProjects.tsx
+++ app/OtherProjects.tsx
@@ -4,7 +4,7 @@ import { Button } from "./components/Button";
 
 export const OtherProjects = () => {
   return (
-    <div className="w-1/2 space-y-4">
+    <div className="w-full px-8 lg:px-0 lg:w-1/2 space-y-4">
       <Button
         variant="outlined"
         text="Download my CV"
