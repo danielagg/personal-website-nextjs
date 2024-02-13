2033561dfa3f296ccaac14c69f84dc0b35342e06
Daniel Agg <daniel.agg@outlook.com>
2024-02-13 22:59:23 +0100

    reduce opacity

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 export const Card = ({ children }: { children: JSX.Element }) => {
   return (
-    <div className="bg-accent p-8 border-l-8 border-primary rounded">
+    <div className="bg-accent p-8 border-l-8 border-primary/50 rounded">
       {children}
     </div>
   );
